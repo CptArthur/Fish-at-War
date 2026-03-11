ed850748a4f72a9b22a51c6e1e410f559dc174bf
Unicorn Consulting <66555795+g46492@users.noreply.github.com>
2026-03-11 22:18:38 +0100

    Add StorageKeys class with GUID constants
    Introduce AaWFoodScript.StorageKeys static class that defines two readonly Guid fields (FISHATWARSETTINGS and FISHATWARNETCONTENT) to be used as stable storage identifiers. Adds new file StorageKeys.cs containing the GUID values.

---
+++ AaWFoodScript/StorageKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AaWFoodScript
+{
+    public static class StorageKeys
+    {
+        public static readonly Guid FISHATWARSETTINGS = new Guid("7275d81b-965f-318b-e20f-efb4c23480b9");
+        public static readonly Guid FISHATWARNETCONTENT = new Guid("7275d81b-965f-318b-e20f-efb4c23480c0");
+    }
+}
